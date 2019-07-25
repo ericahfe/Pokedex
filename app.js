1d1fcba712da373c3268e31e7ff0072d50461b7d
Eric Alvarez <ericalvarez00@hotmail.com>
2019-07-25 17:39:30 -0500

    tercer avance

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const section = document.createElement("section")
-section.classList.add("pokedex")
- document.getElementById("app").appendChild(section);
+const input = document.getElementById("search");
+input.addEventListener("input", (event) => { 
+
+const pokemon = event.target.value;
+who_is_that_pokemon_info(pokemon)
+
+});
+const who_is_that_pokemon_info = (pokemon) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+        .then((response) => response.json())
+
+        .then((call_pokemon) => {
+            console.log(call_pokemon);
+
+        }
+        );
+
+}
