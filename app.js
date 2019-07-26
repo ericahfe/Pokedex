4cadd44ae6bd061491802f501af75283590ed90e
erichafe <ericalvarez00@hotmail.com>
2019-07-26 14:39:46 -0500

    tercer avance

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const input = document.getElementById("search");
+const button = document.getElementById("button");
+
+
+function find () {
+    document.getElementById()
+}
 input.addEventListener("input", (event) => { 
+button.click
 
 const pokemon = event.target.value;
-who_is_that_pokemon_info(pokemon)
+who_is_that_pokemon_info(pokemon);
 
 });
 const who_is_that_pokemon_info = (pokemon) => {
@@ -17,3 +24,4 @@ const who_is_that_pokemon_info = (pokemon) => {
         );
 
 }
+
