12952f89697b90d7f21b461ddda157e5e44f5a69
erichafe <ericalvarez00@hotmail.com>
2019-07-29 11:53:27 -0500

    agregue el input para los pokemon shiny y el codigo para los tipos de pokemon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const pokemonName = document.getElementById("pokemonName");
 const pokemonImage = document.getElementById("pokemonImage");
 const pokemonNumber = document.getElementById("pokemonNumber");
 const pokemonType = document.getElementById("pokemonType")
-
+const checkbox = document.getElementById("checkbox")
 
 button.addEventListener("click", (event) => {
     const valor = input.value;
@@ -17,9 +17,21 @@ const getPokemon = (pokemon) => {
         .then((pokemon) => {
             console.log(pokemon);
             pokemonName.innerHTML = pokemon.name;
-            pokemonImage.setAttribute("src", pokemon.sprites.front_default);
+            //if(document.getElementById("checkbox") = false) {
+                pokemonImage.setAttribute("src", pokemon.sprites.front_default);
+            //}
+            //if(document.getElementById("checkbox") = true) {
+            //    pokemonImage.setAttribute("src", pokemon.sprites.front_shiny);
+            //}
+            
             pokemonNumber.innerHTML = pokemon.id;
-            pokemonType.innerHTML = pokemon.types.type.name;
+            for(let i=0; i < pokemon.types.length; i++) {
+                const pokemonTypeName = document.createElement("p");
+                const pokemonTypeNameInfo = document.createTextNode(pokemon.types[i]);
+                pokemonTypeName.appendChild(pokemonTypeNameInfo)
+                pokemonTypeName.classList.add("pokedex__type__style")
+                pokemonTypeName.appendChild("pokemonContainer")
+            }
         }).catch((error) => {
             alert("Lo sentimos no encontramos ese pokemon")
         })
