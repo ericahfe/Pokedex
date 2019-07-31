ca3f36b0c01886ba05ab4c8fb53ad51153fa205c
erichafe <ericalvarez00@hotmail.com>
2019-07-31 13:57:08 -0500

    se hizo todos los estilos de los tipos de pokemon y empece a trabajar con las evoluciones de los pokemons

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const pokemonImage = document.getElementById("pokemonImage");
 const pokemonNumber = document.getElementById("pokemonNumber");
 const pokemonType = document.getElementById("pokemonType");
 const showShiny = document.getElementById("showShiny");
-
+const pokemonHeight = document.getElementById("pokemonHeight")
+const pokemonWeight = document.getElementById("pokemonWeight")
+const evolutionName1 = document.getElementById("evolutionName1")
+const evolutionImg1 = document.getElementById("evolutionImg1")
 button.addEventListener("click", (event) => {
     const valor = input.value;
     getPokemon(valor);
+    getEvolutions(valor);
 });
 
 const getPokemon = (pokemon) => {
@@ -18,6 +22,9 @@ const getPokemon = (pokemon) => {
             console.log(pokemon);
             pokemonNumber.innerHTML = pokemon.id;
             pokemonName.innerHTML = pokemon.name;
+            
+            pokemonHeight.innerHTML = pokemon.height;
+            pokemonWeight.innerHTML = pokemon.weight;
 
             if (showShiny.checked == true) {
                 pokemonImage.setAttribute("src", pokemon.sprites.front_shiny);
@@ -25,6 +32,16 @@ const getPokemon = (pokemon) => {
                 pokemonImage.setAttribute("src", pokemon.sprites.front_default);
             }
 
+            
+            const getEvolutions = (pokemonID) => {
+                fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokemonID}`)
+                .then((response) => response.json())
+                .then((pokemonID) => {
+                    console.log(pokemonID);
+                    evolutionName1.innerHTML = pokemonID.baby_trigger_item
+                    }
+                )}
+            
 
             const pokemonTypes = pokemon.types; // arreglo del tipo de pokemon
 
@@ -36,11 +53,12 @@ const getPokemon = (pokemon) => {
                 pokemonTypeName.classList.add(`pokedex__type--${pokemonTypes[i].type.name}`);
                 pokemonType.appendChild(pokemonTypeName);
 
+
+
+
             }
         })
 }
 
 
-const getEvolutions = (pokemonID) => {
-
-}
+    
